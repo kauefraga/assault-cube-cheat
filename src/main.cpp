2d1e29d8fc9b30ef898c1bf9206cb64571943252
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-07-04 22:23:36 -0300

    :recycle: chore: rewrite some types, use assign add += to write new health value and separate offsets in other header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <thread>
 #include "memory.h"
-
-namespace offset {
-  const int base_address = 0x18AC00;
-  const int health = 0xEC;
-}
+#include "offsets.h"
 
 int main() {
-  const char* process_name = "ac_client.exe";
+  const LPCTSTR process_name = (LPCTSTR) "ac_client.exe";
 
-  DWORD pid = get_process_id_by_name(TEXT(process_name));
+  DWORD pid = get_process_id_by_name(process_name);
 
   if (pid == 0) {
     std::cout << "Process id could not be retrieved" << '\n';
@@ -19,9 +15,9 @@ int main() {
 
   std::cout << "Process id: " << pid << '\n';
 
-  HANDLE handle_process = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+  HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
 
-  if (!handle_process) {
+  if (!process) {
     std::cout << "Process could not be opened" << '\n';
     return 1;
   }
@@ -35,16 +31,15 @@ int main() {
   }
 
   // get base address of pointer chain (entity object pointer)
-  // ac_client.exe+0018AC00
   uintptr_t dynamic_pointer_base_address = ac_client + offset::base_address;
   std::cout << "Dynamic Pointer Base Address: 0x" << std::hex << dynamic_pointer_base_address << '\n';
 
-  uintptr_t health_address = findDMAA(handle_process, dynamic_pointer_base_address, { offset::health });
-  std::cout << "health address = " << "0x" << std::hex << health_address << std::endl;
+  uintptr_t health_address = findDMAA(process, dynamic_pointer_base_address, { offset::health });
+  std::cout << "health address = " << "0x" << std::hex << health_address << '\n';
 
   // Get the current health
   int health_value = 0;
-  ReadProcessMemory(handle_process, (BYTE*) health_address, &health_value, sizeof(health_value), nullptr);
+  ReadProcessMemory(process, (BYTE*) health_address, &health_value, sizeof(health_value), nullptr);
   std::cout << "Health value: " << std::dec << health_value << '\n';
 
   while (!GetAsyncKeyState(VK_END)) {
@@ -52,8 +47,8 @@ int main() {
 
     if (GetAsyncKeyState(VK_NUMPAD1)) {
       // Write new value to health
-      int new_health_value = 300;
-      WriteProcessMemory(handle_process, (BYTE*)health_address, &new_health_value, sizeof(new_health_value), nullptr);
+      health_value += 300;
+      WriteProcessMemory(process, (BYTE*) health_address, &health_value, sizeof(health_value), nullptr);
     }
   }
 
---
+++ src/offsets.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstdint>
+
+namespace offset {
+  /* ac_client.exe */
+  constexpr uintptr_t base_address = 0x18AC00;
+  constexpr uintptr_t health = 0xEC;
+}
